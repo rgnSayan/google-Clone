a35a5a707be2000077ced874c31dcffbc3316716
SAYAN MONDAL <76264679+rgnSayan@users.noreply.github.com>
2021-07-18 04:02:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/Home';
+import SearchPage from './Components/SearchPage'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/" >
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyDYCP07J5G-sAslLqaAcHE4kfKi_gn400U";
+
+export default API_KEY;
